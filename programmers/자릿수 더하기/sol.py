2060d276b114bd62da505fbdae291614c46361eb
unknown <lemans997@gmail.com>
2023-10-11 16:44:06 +0900

    보충

--- programmers/자릿수 더하기/sol.py
+++ programmers/자릿수 더하기/sol.py
@@ -1,3 +1,4 @@
+# 내가 한것
 def solution(n):
     answer = 0
 
@@ -11,3 +12,27 @@ def solution(n):
     return answer
 
 print(solution)
+
+# def solution(n):
+#     answer = 0
+    
+#     while n > 0:
+#         # a: 몫, b: 나머지
+#         a = n // 10
+#         b = n % 10
+
+#         n = a
+#         answer += b
+
+#     return answer
+
+def solution(n):
+    answer = 0
+    
+    for i in str(n):
+        answer += int(i)
+
+    return answer
+
+print(solution(1234))
+print(solution(930211))
---
+++ programmers/특정 문자 제거하기/sol.py
@@ -0,0 +1,20 @@
+# def solution(my_string, letter):
+#     answer = ''
+
+#     answer = my_string.replace(letter, '')
+
+#     return answer
+
+
+def solution(my_string, letter):
+    answer = ''
+
+    for string in my_string:
+        if string != letter:
+            answer += string
+
+    return answer
+
+
+print(solution("abcdef", "f"))
+print(solution("BCBdbe", "B"))
